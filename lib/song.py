b421bfeee4a4dd88bc0819eff904d77f9b3dd2a4
Salman <salmaanmohamed700@gmail.com>
2025-05-13 11:51:22 +0300

    Done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,45 @@
 class Song:
-    pass
+    # Class attributes to track song count, genres, and artists
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()  # Increment song count
+        Song.add_to_genres(self.genre)  # Add genre to genre list
+        Song.add_to_artists(self.artist)  # Add artist to artist list
+        Song.add_to_genre_count(self.genre)  # Update genre count
+        Song.add_to_artist_count(self.artist)  # Update artist count
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
